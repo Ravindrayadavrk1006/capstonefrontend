41ebb8e8ffd867bef69c0cde39f9fff7dc66e7f7
unknown <ravindrayadavrk1006@gmail.com>
2021-04-11 17:31:05 +0530

    improved products

--- src/components/Products.js
+++ src/components/Products.js
@@ -10,54 +10,49 @@ export default function Products(props) {
 
     return (
       <section>
-        <div style={{margin:'0',}} className='container'>
+        <div
+          style={{ margin: '0', marginRight: '0', boxSizing: 'border-box' }}
+          className='container'
+        >
           <div className='row'>
-            {/* <div className='col-3 sidebarforcategory'>
+            <div className='col-2 '>
               <div className='categoriesOfArt'>
-                <ul class='list-group'>
-                  <li className='list-group-item'>Cras justo odio</li>
-                  <li className='list-group-item'>Dapibus ac facilisis in</li>
-                  <li className='list-group-item'>Morbi leo risus</li>
-                  <li className='list-group-item'>Porta ac consectetur ac</li>
-                  <li className='list-group-item'>Vestibulum at eros</li>
+                <ul style={{ position: 'sticky' }} className='list-group'>
+                  <li className='art-category-item list-group-item'>Pottery</li>
+                  <li className='art-category-item list-group-item'>
+                    Artifacts
+                  </li>
+                  <li className='art-category-item list-group-item'>
+                    Paintings
+                  </li>
+                  <li className='art-category-item list-group-item'>
+                    Handicrafts
+                  </li>
+                  <li className='art-category-item list-group-item'>Others</li>
                 </ul>
               </div>
-            </div> */}
-            <div className='col'>
+            </div>
+            <div className='col-10'>
               <div className='products'>
-                {products
-                  .filter((product) => {
-                    if (
-                      product.title
-                        .toLowerCase()
-                        .includes(props.search.toLowerCase())
-                    ) {
-                      return product
-                    } else {
-                      return false
-                    }
-                  })
-                  .map((product) => (
-                    <div className='products-card' key={product.pid}>
-                      <Link to={`/products/${product.pid}`}>
-                        <img src={product.images[0]} alt='cover-pic' />
-                      </Link>
-                      <div className='products-content'>
-                        <h3 title={product.title}>
-                          <Link to={`/products/${product.pid}`}>
-                            {product.title}
-                          </Link>
-                        </h3>
-                        <p className='products-desc'>{product.description}</p>
-                        <p className='products-price'>
-                          &#8377; {product.price}
-                        </p>
-                        <button onClick={() => addCart(product.pid)}>
-                          Add to Cart
-                        </button>
-                      </div>
+                {products.map((product) => (
+                  <div className='products-card' key={product.pid}>
+                    <Link to={`/products/${product.pid}`}>
+                      <img src={product.images[0]} alt='cover-pic' />
+                    </Link>
+                    <div className='products-content'>
+                      <h3 title={product.title}>
+                        <Link to={`/products/${product.pid}`}>
+                          {product.title}
+                        </Link>
+                      </h3>
+                      <p className='products-desc'>{product.description}</p>
+                      <p className='products-price'>&#8377; {product.price}</p>
+                      <button onClick={() => addCart(product.pid)}>
+                        Add to Cart
+                      </button>
                     </div>
-                  ))}
+                  </div>
+                ))}
               </div>
             </div>
           </div>
